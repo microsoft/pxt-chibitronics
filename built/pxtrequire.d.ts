7568d6097682ef84a69a034d4eb6f278f4b98938
Guillaume Jenkins <gujenkin@microsoft.com>
2017-03-14 13:11:22 -0700

    Delete built files from repo and add Jenkins scripts

--- built/pxtrequire.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../node_modules/pxt-core/typings/globals/node/index.d.ts" />
-/// <reference path="../node_modules/pxt-core/built/pxtlib.d.ts" />
-export declare let pxtCore: any;
-export declare let pxtCoreDir: string;
