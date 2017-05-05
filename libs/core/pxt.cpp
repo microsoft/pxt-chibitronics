d158a3a32c1048460b0fdd4cc2747ca38931ad2c
Sam El-Husseini <samelh@microsoft.com>
2017-05-05 02:24:13 -0700

    Fix pxt.cpp

--- libs/core/pxt.cpp
+++ libs/core/pxt.cpp
@@ -1,7 +1,11 @@
 #include "pxt.h"
-#include <map>
+#ifdef printf
+#undef printf
+#define printf(...)
+#endif
 
-MicroBit uBit;
+static const UT_mm _utmm_uint64 = {.sz = sizeof(struct pxt::MapEntry), 0, 0, 0, 0};
+const UT_mm* utmm_uint64 = &_utmm_uint64;
 
 namespace pxt {
     int incr(uint32_t e)
@@ -117,7 +121,9 @@ namespace pxt {
 
     void RefObject::destroy() {
       ((RefObjectMethod)getVTable()->methods[0])(this);
+      delete this;
     }
+
 
     void RefObject::print() {
       ((RefObjectMethod)getVTable()->methods[1])(this);
@@ -131,9 +137,6 @@ namespace pxt {
           if (refmask[i]) decr(r->fields[i]);
           r->fields[i] = 0;
         }
-        //RefRecord is allocated using placement new
-        r->~RefRecord();
-        ::operator delete(r);
     }
 
     void RefRecord_print(RefRecord *r)
@@ -141,327 +144,99 @@ namespace pxt {
       printf("RefRecord %p r=%d size=%d bytes\n", r, r->refcnt, r->getVTable()->numbytes);
     }
 
-    uint32_t Segment::get(uint32_t i)
-    {
-#ifdef DEBUG_BUILD
-      printf("In Segment::get index:%u\n", i);
-      this->print();
-#endif            
-      
-      if (i < length)
-      {
-          return data[i];          
-      }
-      return Segment::DefaultValue;
-    }
-
-    void Segment::set(uint32_t i, uint32_t value) 
-    {
-        if (i < size)
-        {
-          data[i] = value;
-        }
-        else if (i < Segment::MaxSize)
-        {
-          growByMin(i + 1);
-          data[i] = value;
-        }
-        if (length <= i)
-        {
-           length = i + 1; 
-        }        
-
-#ifdef DEBUG_BUILD
-        printf("In Segment::set\n");
-        this->print();
-#endif            
-        
-        return;
-    }      
-
-    uint16_t Segment::growthFactor(uint16_t size)
-    {
-      if (size == 0)
-      {
-        return 4;
-      }
-      if (size < 64)
-      {
-        return size * 2; // Double
-      }
-      if (size < 512)
-      {
-        return size * 5/3; //Grow by 1.66 rate
-      }
-      return size + 256; //Grow by constant rate
-    }
-
-    void Segment::growByMin(uint16_t minSize)
-    {
-      growBy(max(minSize, growthFactor(size)));
-    }
-
-    void Segment::growBy(uint16_t newSize) 
-    {
-#ifdef DEBUG_BUILD
-         printf("growBy: %d\n", newSize);
-         this->print();
-#endif      
-      if (size < newSize)
-      {
-         //this will throw if unable to allocate
-         uint32_t *tmp = (uint32_t*)(::operator new(newSize * sizeof(uint32_t)));
-
-         //Copy existing data
-         if (size)
-         {
-           memcpy(tmp, data, size * sizeof(uint32_t));
-         }
-        //fill the rest with default value
-         memset(tmp + size, Segment::DefaultValue, (newSize - size) * sizeof(uint32_t));
-
-         //free older segment;
-         ::operator delete(data); 
-
-         data = tmp; 
-         size = newSize;
-
-#ifdef DEBUG_BUILD
-         printf("growBy - after reallocation\n");
-         this->print();
-#endif         
-         
-      }
-      //else { no shrinking yet; }
-      return;
-    }
-
-    void Segment::ensure(uint16_t newSize)
-    {
-      if (newSize < size)
-      {
-        return;
-      }
-      growByMin(newSize);
-    }
-
-    void Segment::setLength(uint32_t newLength)
-    {
-      if (newLength > size)
-      {
-        ensure(length);        
-      }
-      length = newLength;
-      return;
-    }
-
-    void Segment::push(uint32_t value) 
-    { 
-      this->set(length, value);
-    }
-    
-    uint32_t Segment::pop() 
-    {
-#ifdef DEBUG_BUILD
-      printf("In Segment::pop\n");
-      this->print();
-#endif            
-      
-      if (length > 0)
-      {
-        uint32_t value = data[length];
-        data[length] = Segment::DefaultValue;
-        --length;
-        return value;
-      }
-      return Segment::DefaultValue;
-    }
-
-    //this function removes an element at index i and shifts the rest of the elements to
-    //left to fill the gap   
-    uint32_t Segment::remove(uint32_t i) 
-    {
-#ifdef DEBUG_BUILD
-      printf("In Segment::remove index:%u\n", i);
-      this->print();
-#endif                  
-      if (i < length)
-      {
-        //value to return
-        uint32_t ret = data[i];
-        if (i + 1 < length)
-        {
-          //Move the rest of the elements to fill in the gap.
-          memmove(data + i, data + i + 1, (length - i - 1) * sizeof(uint32_t));
-        }
-        length--;        
-        data[length] = Segment::DefaultValue;        
-#ifdef DEBUG_BUILD
-        printf("After Segment::remove index:%u\n", i);
-        this->print();
-#endif  
-        return ret;
-      }
-      return Segment::DefaultValue;
-    }
-
-    //this function inserts element value at index i by shifting the rest of the elements right.     
-    void Segment::insert(uint32_t i, uint32_t value) 
-    {
-#ifdef DEBUG_BUILD
-      printf("In Segment::insert index:%u value:%u\n", i, value);
-      this->print();
-#endif                  
-
-      if (i < length)
-      {
-        ensure(length + 1);
-        if (i + 1 < length)
-        {
-          //Move the rest of the elements to fill in the gap.
-          memmove(data + i + 1, data + i, (length - i) * sizeof(uint32_t));
-        }
-
-        data[i] = value;        
-        length++;
-      }
-      else
-      {
-        //This is insert beyond the length, just call set which will adjust the length
-        set(i, value);
-      }
-#ifdef DEBUG_BUILD
-      printf("After Segment::insert index:%u\n", i);
-      this->print();
-#endif                   
-    }
-
-    void Segment::print()
-    {
-      printf("Segment: %x, length: %u, size: %u\n", data, (uint32_t)length, (uint32_t)size);
-      for(uint32_t i = 0; i < size; i++)
-      {
-        printf("%d ",(uint32_t)data[i]);
-      }
-      printf("\n");
-    }
-
-    bool Segment::isValidIndex(uint32_t i)
-    {
-      if (i > length)
-      {
-        return false;
-      }
-      return true;
-    }
-
-    void Segment::destroy()
-    {
-#ifdef DEBUG_BUILD
-      printf("In Segment::destroy\n");
-      this->print();
-#endif          
-      length = size = 0;
-      ::operator delete(data);
-      data = nullptr;
-    }
-
-    void RefCollection::push(uint32_t x) 
-    {
+    void RefCollection::push(uint32_t x) {
       if (isRef()) incr(x);
-      head.push(x);
+      utvector_push(data, &x);
     }
 
-    uint32_t RefCollection::pop() 
-    {
-      uint32_t ret = head.pop();
-      if (isRef())
-      {
-        incr(ret);  
-      } 
-      return ret;
+    void RefCollection::setLength(int newLength) {
+      utvector_reserve(data, newLength);
     }
 
-    uint32_t RefCollection::getAt(int i) 
-    {
-      uint32_t tmp = head.get(i);
-      if (isRef())
-      {
-        incr(tmp);
+    uint32_t RefCollection::pop(void) {
+      return removeElement(length() - 1);
+    }
+
+    uint32_t RefCollection::getAt(int x) {
+      if (in_range(x)) {
+        uint32_t tmp = *(uint32_t *)utvector_elt(data, (unsigned)x);
+        if (isRef()) incr(tmp);
+        return tmp;
       }
-      return tmp;
-    }
-
-    uint32_t RefCollection::removeAt(int i) 
-    {
-      if (isRef())
-      {
-        decr(head.get(i));
-      } 
-      return head.remove(i);
-    }
-
-    void RefCollection::insertAt(int i, uint32_t value) 
-    {
-      head.insert(i, value);
-      if (isRef())
-      {
-        incr(value);
-      } 
-    }
-
-    void RefCollection::setAt(int i, uint32_t value) 
-    {
-      if (isRef()) 
-      {
-        if (head.isValidIndex((uint32_t)i))
-        {
-          decr(head.get(i));
-        }
-        incr(value);
+      else {
+        error(ERR_OUT_OF_BOUNDS);
+        return 0;
       }
-      head.set(i, value);
     }
 
-    int RefCollection::indexOf(uint32_t x, int start) 
-    {
-      if (isString()) 
-      {
+    uint32_t RefCollection::removeAt(int x) {
+      uint32_t *ep = (uint32_t *)utvector_elt(data, x);
+      if (!in_range(x))
+        return 0;
+
+      if (!ep)
+        return 0;
+
+      uint32_t e = *ep;
+
+      if (isRef()) decr(e);
+      utvector_erase(data, x);
+      return e;
+    }
+
+    void RefCollection::setAt(int x, uint32_t y) {
+      if (!in_range(x))
+        return;
+
+      if (isRef()) {
+        decr(*(uint32_t *)utvector_elt(data, (unsigned)x));
+        incr(y);
+      }
+      *(uint32_t *)utvector_elt(data, (unsigned)x) = y;
+    }
+
+    void RefCollection::insertAt(int x, uint32_t y) {
+      if (x == length()) {
+        push(y);
+        return;
+      }
+
+      if (!in_range(x))
+        return;
+
+      push(0);
+      int i;
+      for (i = length(); i < x; i--)
+        *(uint32_t *)utvector_elt(data, i) = *(uint32_t *)utvector_elt(data, i - 1);
+      if (isRef()) {
+        incr(y);
+      }
+      *(uint32_t *)utvector_elt(data, x) = y;
+    }
+
+    int RefCollection::indexOf(uint32_t x, int start) {
+      if (!in_range(start))
+        return -1;
+
+      if (isString()) {
+/*
         StringData *xx = (StringData*)x;
-        uint32_t i = start;
-        while(head.isValidIndex(i))
-        {
-          StringData *ee = (StringData*)head.get(i);
-          if (ee == xx)
-          {
-            //handles ee being null
-            return (int) i;
-          }
-          if (ee && xx->len == ee->len && memcmp(xx->data, ee->data, xx->len) == 0)
-          {
+        for (uint32_t i = start; i < data.size(); ++i) {
+          StringData *ee = (StringData*)data.at(i);
+          if (xx->len == ee->len && memcmp(xx->data, ee->data, xx->len) == 0)
             return (int)i;
-          }
-          i++;
         }
-      } 
-      else 
-      {
-        uint32_t i = start;
-        while(head.isValidIndex(i))
-        {
-          if (head.get(i) == x)
-          {
+*/
+      } else {
+        for (uint32_t i = start; i < utvector_len(data); ++i)
+          if (*((uint32_t *)utvector_elt(data, i)) == x)
             return (int)i;
-          }
-          i++;
-        }
       }
 
       return -1;
     }
 
-    int RefCollection::removeElement(uint32_t x) 
-    {
+    int RefCollection::removeElement(uint32_t x) {
       int idx = indexOf(x, 0);
       if (idx >= 0) {
         removeAt(idx);
@@ -484,6 +259,8 @@ namespace pxt {
     }
 
     RefCollection::RefCollection(uint16_t flags) : RefObject(0) {
+
+      data = utvector_new(utmm_int);
       switch (flags) {
         case 0:
           vtable = PXT_VTABLE_TO_INT(&Coll0::RefCollection_vtable);
@@ -503,20 +280,15 @@ namespace pxt {
     void RefCollection::destroy()
     {
       if (this->isRef())
-      {
-        for(uint32_t i = 0; i < this->head.getLength(); i++)
-        {
-          decr(this->head.get(i));
+        for (uint32_t i = 0; i < utvector_len(this->data); ++i) {
+          decr(*(uint32_t *)utvector_elt(this->data, i));
         }
-      }
-      this->head.destroy();
-      delete this;
+      utvector_clear(this->data);
     }
 
     void RefCollection::print()
     {
-      printf("RefCollection %p r=%d flags=%d size=%d\n", this, refcnt, getFlags(), head.getLength());
-      head.print();
+      printf("RefCollection %p r=%d flags=%d size=%d [%p, ...]\n", this, refcnt, getFlags(), utvector_len(data), utvector_len(data) > 0 ? utvector_elt(data, 0) : 0);
     }
 
     PXT_VTABLE_CTOR(RefAction) {}
@@ -528,9 +300,6 @@ namespace pxt {
         decr(fields[i]);
         fields[i] = 0;
       }
-      //RefAction is allocated using placement new
-      this->~RefAction();
-      ::operator delete(this);
     }
 
     void RefAction::print()
@@ -545,7 +314,6 @@ namespace pxt {
 
     void RefLocal::destroy()
     {
-      delete this;
     }
 
     PXT_VTABLE_CTOR(RefLocal) {
@@ -556,35 +324,25 @@ namespace pxt {
       v = 0;
     }
 
-    void RefRefLocal::print()
-    {
-      printf("RefRefLocal %p r=%d v=%p\n", this, refcnt, (void*)v);
-    }
-
-    void RefRefLocal::destroy()
-    {
-      decr(v);
-      delete this;
-    }
-
     PXT_VTABLE_BEGIN(RefMap, 0, RefMapMarker)
     PXT_VTABLE_END
-    RefMap::RefMap() : PXT_VTABLE_INIT(RefMap) {}
+    RefMap::RefMap() : PXT_VTABLE_INIT(RefMap) {
+      data = utvector_new(utmm_uint64);
+    }
 
     void RefMap::destroy() {
-      for (unsigned i = 0; i < data.size(); ++i) {
-        if (data[i].key & 1) {
-          decr(data[i].val);
+      for (unsigned i = 0; i < utvector_len(data); ++i) {
+        if (((struct MapEntry *)utvector_elt(data, i))->key & 1) {
+          decr(((struct MapEntry *)utvector_elt(data, i))->val);
         }
-        data[i].val = 0;
+        ((struct MapEntry *)utvector_elt(data, i))->val = 0;
       }
-      data.resize(0);
-      delete this;
+      utvector_clear(data);
     }
 
     int RefMap::findIdx(uint32_t key) {
-      for (unsigned i = 0; i < data.size(); ++i) {
-        if (data[i].key >> 1 == key)
+      for (unsigned i = 0; i < utvector_len(data); ++i) {
+        if (((struct MapEntry *)utvector_elt(data, i))->key >> 1 == key)
           return i;
       }
       return -1;
@@ -592,157 +350,32 @@ namespace pxt {
 
     void RefMap::print()
     {
-      printf("RefMap %p r=%d size=%d\n", this, refcnt, data.size());
+      printf("RefMap %p r=%d size=%d\n", this, refcnt, utvector_len(data));
     }
 
+    void debugMemLeaks() {}
 
-#ifdef DEBUG_MEMLEAKS
-  std::set<RefObject*> allptrs;
-  void debugMemLeaks()
-  {
-    printf("LIVE POINTERS:\n");
-    for(std::set<RefObject*>::iterator itr = allptrs.begin();itr!=allptrs.end();itr++)
+    uint32_t *allocate(uint16_t sz) {
+      uint32_t *arr;
+
+      arr = (uint32_t *) malloc(sz * sizeof(*arr));
+      memset((void *)arr, 0, sz * sizeof(*arr));
+
+      return arr;
+    }
+
+    int templateHash()
     {
-      (*itr)->print();
-    }
-    printf("\n");
-  }
-#else
-  void debugMemLeaks() {}
-#endif
-
-
-    // ---------------------------------------------------------------------------
-    // An adapter for the API expected by the run-time.
-    // ---------------------------------------------------------------------------
-
-    map<pair<int, int>, Action> handlersMap;
-
-    MicroBitEvent lastEvent;
-
-    // We have the invariant that if [dispatchEvent] is registered against the DAL
-    // for a given event, then [handlersMap] contains a valid entry for that
-    // event.
-    void dispatchEvent(MicroBitEvent e) {
-
-      lastEvent = e;
-
-      Action curr = handlersMap[{ e.source, e.value }];
-      if (curr)
-        runAction1(curr, e.value);
-
-      curr = handlersMap[{ e.source, MICROBIT_EVT_ANY }];
-      if (curr)
-        runAction1(curr, e.value);
+      return ((int*)bytecode)[4];
     }
 
-    void registerWithDal(int id, int event, Action a) {
-      Action prev = handlersMap[{ id, event }];
-      if (prev)
-        decr(prev);
-      else
-        uBit.messageBus.listen(id, event, dispatchEvent);
-      incr(a);
-      handlersMap[{ id, event }] = a;
-    }
-
-    void fiberDone(void *a)
+    int programHash()
     {
-      decr((Action)a);
-      release_fiber();
+      return ((int*)bytecode)[6];
     }
 
-
-    void runInBackground(Action a) {
-      if (a != 0) {
-        incr(a);
-        create_fiber((void(*)(void*))runAction0, (void*)a, fiberDone);
-      }
+    int getNumGlobals()
+    {
+      return bytecode[16];
     }
-
-
-  void error(ERROR code, int subcode)
-  {
-    printf("Error: %d [%d]\n", code, subcode);
-    uBit.panic(42);
-  }
-
-  uint16_t *bytecode;
-  uint32_t *globals;
-  int numGlobals;
-
-  uint32_t *allocate(uint16_t sz)
-  {
-    uint32_t *arr = new uint32_t[sz];
-    memset(arr, 0, sz * 4);
-    return arr;
-  }
-
-  void checkStr(bool cond, const char *msg)
-  {
-    if (!cond) {
-      while (true) {
-        uBit.display.scroll(msg, 100);
-        uBit.sleep(100);
-      }
-    }
-  }
-
-  int templateHash()
-  {
-    return ((int*)bytecode)[4];
-  }
-
-  int programHash()
-  {
-    return ((int*)bytecode)[6];
-  }
-
-  int getNumGlobals()
-  {
-    return bytecode[16];
-  }
-
-  void exec_binary(int32_t *pc)
-  {
-    // XXX re-enable once the calibration code is fixed and [editor/embedded.ts]
-    // properly prepends a call to [internal_main].
-    // ::touch_develop::internal_main();
-
-    // unique group for radio based on source hash
-    // ::touch_develop::micro_bit::radioDefaultGroup = programHash();
-
-    // repeat error 4 times and restart as needed
-    microbit_panic_timeout(4);
-
-    int32_t ver = *pc++;
-    checkStr(ver == 0x4209, ":( Bad runtime version");
-
-    bytecode = *((uint16_t**)pc++);  // the actual bytecode is here
-    globals = allocate(getNumGlobals());
-
-    // just compare the first word
-    checkStr(((uint32_t*)bytecode)[0] == 0x923B8E70 &&
-             templateHash() == *pc,
-             ":( Failed partial flash");
-
-    uint32_t startptr = (uint32_t)bytecode;
-    startptr += 48; // header
-    startptr |= 1; // Thumb state
-
-    ((uint32_t (*)())startptr)();
-
-#ifdef DEBUG_MEMLEAKS
-    pxt::debugMemLeaks();
-#endif
-
-    return;
-  }
-
-  void start()
-  {
-    exec_binary((int32_t*)functionsAndBytecode);
-  }
 }
-
-// vim: ts=2 sw=2 expandtab
