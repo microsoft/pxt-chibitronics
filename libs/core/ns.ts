672627a2bc805234d9e47f6be23261e3ec02ce2b
Sam El-Husseini <samelh@microsoft.com>
2017-05-09 20:49:34 -0700

    Updating icons.

--- libs/core/ns.ts
+++ libs/core/ns.ts
@@ -9,7 +9,7 @@ namespace control {
 /**
  * Control currents in Pins for analog/digital signals, servos, i2c, ...
  */
-//% color=#4C97FF weight=100 icon="\uf140"
+//% color=#4C97FF weight=100 icon="\uf185"
 namespace lights {
 
 }
@@ -17,7 +17,7 @@ namespace lights {
 /**
  * Control currents in Pins for analog/digital signals, servos, i2c, ...
  */
-//% color=#9966FF weight=90 icon="\uf140"
+//% color=#9966FF weight=90 icon="\uf192"
 namespace sensing {
 
 }
