1b0f486ae8ced71b727c094cef3458f4e3da71d0
Sean Cross <sean@xobs.io>
2017-07-05 14:20:12 +0800

    variant: run source file through formatter
    Signed-off-by: Sean Cross <sean@xobs.io>

--- libs/core/variant.cpp
+++ libs/core/variant.cpp
@@ -35,9 +35,10 @@ int error_subcode = 0;
 void *malloc(size_t size);
 void *memset(void *s, int c, size_t n);
 
-static uint32_t *allocate(uint16_t sz) {
+static uint32_t *allocate(uint16_t sz)
+{
   uint32_t *arr;
-  
+
   arr = (uint32_t *)malloc(sz * sizeof(*arr));
   memset(arr, 0, sz * sizeof(*arr));
 
@@ -46,36 +47,39 @@ static uint32_t *allocate(uint16_t sz) {
 
 static int templateHash(void)
 {
-  return ((int*)bytecode)[4];
+  return ((int *)bytecode)[4];
 }
 
-static int getNumGlobals(void) {
+static int getNumGlobals(void)
+{
   return bytecode[16];
 }
 
-__attribute__((noreturn))
-void panic(const char *str) {
+__attribute__((noreturn)) void panic(const char *str)
+{
   panic_msg = str;
   setSerialSpeed(9600);
   while (1)
     printf("PANIC: %s\r\n", str);
 }
 
-__attribute__((noreturn))
-void error(ERROR code, int subcode) {
+__attribute__((noreturn)) void error(ERROR code, int subcode)
+{
   error_code = code;
   error_subcode = subcode;
   setSerialSpeed(9600);
-  while(1)
+  while (1)
     printf("ERROR %d: %d\r\n", code, subcode);
 }
 
-static void assert(int cond, const char *msg) {
+static void assert(int cond, const char *msg)
+{
   if (!cond)
     panic(msg);
 }
 
-static void exec_binary(int32_t *pc) {
+static void exec_binary(int32_t *pc)
+{
 
   // XXX re-enable once the calibration code is fixed and [editor/embedded.ts]
   // properly prepends a call to [internal_main].
@@ -83,34 +87,35 @@ static void exec_binary(int32_t *pc) {
 
   // unique group for radio based on source hash
   // ::touch_develop::micro_bit::radioDefaultGroup = programHash();
-    
+
   // repeat error 4 times and restart as needed
   // microbit_panic_timeout(4);
-    
+
   int32_t ver = *pc++;
   assert(ver == 0x4209, ":( Bad runtime version");
 
-  bytecode = *((uint16_t**)pc++);  // the actual bytecode is here
+  bytecode = *((uint16_t **)pc++); // the actual bytecode is here
   globals = allocate(getNumGlobals());
 
   // just compare the first word
-  assert(((uint32_t*)bytecode)[0] == 0x923B8E70 &&
-           templateHash() == *pc,
-           ":( Failed partial flash");
+  assert(((uint32_t *)bytecode)[0] == 0x923B8E70 &&
+             templateHash() == *pc,
+         ":( Failed partial flash");
 
   uint32_t startptr = (uint32_t)bytecode;
   startptr += 48; // header
-  startptr |= 1; // Thumb state
+  startptr |= 1;  // Thumb state
 
-  ((uint32_t (*)())startptr)();
+  ((uint32_t(*)())startptr)();
 
   return;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
   (void)argc;
   (void)argv;
-  exec_binary((int32_t*)functionsAndBytecode);
+  exec_binary((int32_t *)functionsAndBytecode);
   exitThread(0);
   return 0;
 }
