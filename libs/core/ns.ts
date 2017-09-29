c80909b23bb6ab812126e4845be1ec827ea06220
Sam El-Husseini <samelh@microsoft.com>
2017-09-29 15:51:53 -0400

    Using number dropdown field picker for the pause block

--- libs/core/ns.ts
+++ libs/core/ns.ts
@@ -37,6 +37,18 @@ namespace scope {
 //% color=#0078D7 weight=70 icon="\uf110"
 namespace loops {
 
+    /**
+      * Get the time field editor
+      * @param ms how long to pause for, eg: 100, 200, 500, 1000, 2000
+      */
+    //% blockId=timePicker block="%ms"
+    //% blockHidden=true
+    //% shim=TD_ID colorSecondary="#FFFFFF"
+    //% ms.fieldEditor="numberdropdown" ms.fieldOptions.decompileLiterals=true
+    //% ms.fieldOptions.data='[["100 ms", 100], ["200 ms", 200], ["500 ms", 500], ["1 second", 1000], ["2 seconds", 2000]]'
+    export function timePicker(ms: number): number{
+        return ms;
+    }
 }
 
 /**
