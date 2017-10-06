691b0e338f18bd955742d6d61f0d44d8526444da
Sam El-Husseini <microsoftsam@users.noreply.github.com>
2017-10-06 13:09:23 -0400

    Built in effects (#98)
    * Built in effects
    
    * Fix tempo
    
    * Make twinkle less blocking

--- libs/core/lights.ts
+++ libs/core/lights.ts
@@ -1,3 +1,19 @@
+
+/**
+ * Pre-defined effects
+ */
+enum Effect {
+    //% blockImage=1
+    //% blockIdentity=lights.effectPicker
+    Twinkle = 0,
+    //% blockImage=1
+    //% blockIdentity=lights.effectPicker
+    Heartbeat = 1,
+    //% blockImage=1
+    //% blockIdentity=lights.effectPicker
+    Blink = 2
+}
+
 namespace lights {
     
         /**
@@ -60,5 +76,75 @@ namespace lights {
         export function set(name: number, value: number) {
             pins.analogPinMode(name, PinMode.Output);
             pins.analogWrite(name, Math.map(value, 0, 100, 0, 1023));
+        }
+
+        /**
+         * Show a predefined effect on the specified pin
+         * @param name pin name to write to, eg: AnalogPin.A0
+         * @param value effect to show, eg: Effect.Twinkle
+         */
+        //% help=lights/animate weight=22
+        //% blockId=lights_effect block="show %value=lights_effectPicker| effect on %name=analog_pin"
+        export function effect(value: number, name: number, tempo?: number) {
+            switch(value) {
+                case 0: // Twinkle
+                    if (tempo) twinkle(name, tempo);
+                    else twinkle(name);
+                    break;
+                case 1: // Heartbeat
+                    if (tempo) heartbeat(name, tempo);
+                    else heartbeat(name);
+                    break;
+                case 2: // Blink
+                    if (tempo) blink(name, tempo);
+                    else blink(name);
+                    break;
+            }
+        }
+
+        //% blockId=lights_effectPicker block="%value"
+        //% value.fieldEditor="imagedropdown" value.fieldOptions.columns=3
+        //% value.fieldOptions.decompileLiterals=true
+        //% shim=TD_ID blockHidden=1
+        export function effectPicker(value: Effect): number {
+            return value;
+        }
+
+        export function heartbeat(pin: AnalogPin, tempo = 12) {
+            let current = 0;
+            if (tempo > 12)
+                tempo = 12;
+
+            current = fade_to(current, 0xc0, 2, pin, 1);
+            current = fade_to(current, 0x4, 2, pin, 1);
+            loops.pause(80); // fastest rate
+            loops.pause((13 - tempo) * 15);
+            //delay(180);
+            current = fade_to(current, 0xff, 2, pin, 1);
+            current = fade_to(current, 0x00, 2, pin, 1);
+            loops.pause(214); // fastest rate
+            loops.pause((13 - tempo) * 37);
+            //delay(420);
+        }
+
+        export function twinkle(pin: AnalogPin, tempo = 5) {
+            let current = 128;
+            for (let i = 0; i < tempo * 2; i++) {
+                current = fade_to(current, Math.randomRange(0, 255), tempo, pin, 3);
+            }
+        }
+
+        export function blink(pin: AnalogPin, tempo = 4) {
+            fade_to(0, 255, tempo, pin, 7);
+            fade_to(255, 0, tempo, pin, 7);
+        }
+
+        function fade_to(current: number, target: number, rate: number, pin: AnalogPin, pause: number) {
+            while (Math.abs(current - target) > rate) {
+                pins.analogWrite(pin, current);
+                current = current + ((target - current) > 0 ? rate : - rate);
+                loops.pause(pause);
+            }
+            return current;
         }
     }
