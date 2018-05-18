47617af2b0dc789a9090194c61c93554554820ce
Peli de Halleux <pelikhan@users.noreply.github.com>
2018-05-18 13:11:42 -0700

    fixing rgb decompilation issues

--- libs/core/rgb.ts
+++ libs/core/rgb.ts
@@ -89,9 +89,6 @@ namespace rgb {
     //% blockId="rgb_rgbcolor" block="red %red|green %green|blue %blue"
     //% red.min=0 red.max=255 green.min=0 green.max=255 blue.min=0 blue.max=255
     //% weight=20 help="rgb/rgb" blockGap=8 group="Colors"
-    //% red.shadowOptions.color="#FF6680"
-    //% green.shadowOptions.color="#59C059"
-    //% blue.shadowOptions.color="#4C97FF"
     export function rgb(red: number, green: number, blue: number): number {
         return ((red & 0xFF) << 16) | ((green & 0xFF) << 8) | (blue & 0xFF);
     }
